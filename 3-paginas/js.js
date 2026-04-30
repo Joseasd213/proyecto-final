cdd44a01ed393e2a32bfec3be5891b98a86aa1b0
Joseasd213 <baijiachen@fpllefia.com>
2026-04-30 12:57:45 +0200

    "sliders-track-height-needs-tobe-fixed"

--- 3-paginas/js.js
+++ 3-paginas/js.js
@@ -3,7 +3,7 @@ const next = document.querySelector('.right');
 const prev = document.querySelector('.left');
 
 next.addEventListener('click', function() {
-    track.scrollBy({ left: 420, behavior: 'smooth' });
+    track.scrollBy({ left: 420, behavior: 'smooth' }); //Todo depende del signo del número, no del nombre de la propiedad.
 });
 
 prev.addEventListener('click', function() {
